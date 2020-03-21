2e31cc6f02a231e81588e05308146354a893b6d2
GeorgScheumann <53917226+GeorgScheumann@users.noreply.github.com>
2020-03-21 18:25:47 +0100

    Default Icons, names can contain spaces
    Names of the created starter can now contain spaces, also backup Icons are chosen, if the user doesn't select one.

--- gnome_app_adder_0.1.sh
+++ gnome_app_adder_0.1.sh
@@ -9,30 +9,44 @@ if ! EXEC=$(zenity --file-selection --title="Datei auswählen"); then
     exit;
 fi
 echo "exec ok"
-if ! ICON=$(zenity --file-selection --title="Icon auswählen"); then
-    exit;
+if ! ICON=$(zenity --file-selection --title="Icon auswählen")
+	 then
+		if [[ $ICON=="" ]]
+			then
+	 		echo "Kein Icon ausgewählt, default Skaliebares Icon wird benutzt (funktioniert nur unter GNOME!)"
+			MIME=$(xdg-mime query filetype "$EXEC")
+			echo $MIME
+				case "$MIME" in
+					audio/*) ICON=/usr/share/icons/Adwaita/scalable/mimetypes/audio-x-generic-symbolic.svg
+					;;
+					application/*) ICON=/usr/share/icons/Adwaita/scalable/mimetypes/application-x-executable-symbolic.svg
+					;;
+					image/*) ICON=/usr/share/icons/Adwaita/scalable/mimetypes/image-x-generic-symbolic.svg
+					;;
+					video/*) ICON=/usr/share/icons/Adwaita/scalable/mimetypes/video-x-generic-symbolic.svg
+					;;
+
+				esac
+		fi
+
 fi
-echo "icon ok"
-
-
-
 
 
 #create desktop file and fill it up, open the software with MIME-TYPE
 echo "creating .desktop file"
 touch ~/$NAME.desktop 
 echo "file created"
-echo "[Desktop Entry]"                  >> ~/$NAME.desktop
-echo "Name=$NAME"                       >> ~/$NAME.desktop
-echo "Exec=xdg-open ${EXEC// /\\ }"     >> ~/$NAME.desktop
-echo "Icon=${ICON// /\\ }"              >> ~/$NAME.desktop
-echo "Terminal=false"                   >> ~/$NAME.desktop
-echo "Type=Application"                 >> ~/$NAME.desktop
+echo "[Desktop Entry]"                  >> ~/"$NAME".desktop
+echo "Name=$NAME"                       >> ~/"$NAME".desktop
+echo "Exec=xdg-open ${EXEC// /\\ }"     >> ~/"$NAME".desktop
+echo "Icon=${ICON// /\\ }"              >> ~/"$NAME".desktop
+echo "Terminal=false"                   >> ~/"$NAME".desktop
+echo "Type=Application"                 >> ~/"$NAME".desktop
 echo "file creation done"
 
 #move file to ~/.local/share/applications and set the executable bit
-chmod +x $HOME/$NAME.desktop
-mv $HOME/$NAME.desktop $HOME/.local/share/applications
+chmod +x $HOME/"$NAME".desktop
+mv $HOME/"$NAME".desktop $HOME/.local/share/applications
 echo "file moved"
 
 zenity --info --text "Alles erledigt!"
