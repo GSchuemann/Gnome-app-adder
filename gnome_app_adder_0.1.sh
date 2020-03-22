0bc02577bac8ba993d1329e0ba6bb2abceed77c3
GeorgScheumann <53917226+GeorgScheumann@users.noreply.github.com>
2020-03-22 19:01:13 +0100

    regained the abillity to add shell scripts
    The script can now add shell scripts as starters again, it uses ./ for that, so it requires a shebang line in the script.

--- gnome_app_adder_0.1.sh
+++ gnome_app_adder_0.1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#Collecting infos via zenity
-
+#Collecting infos via zenit
+PROGRAM="xdg-open "
+TERMINAL=false
 if ! NAME=$(zenity --entry --text "Name der im Menü angezeigt werden soll" --title "Bezeichnung"); then
   exit;
 fi
@@ -25,22 +26,43 @@ if ! ICON=$(zenity --file-selection --title="Icon auswählen")
 					;;
 					video/*) ICON=/usr/share/icons/Adwaita/scalable/mimetypes/video-x-generic-symbolic.svg
 					;;
-
 				esac
+###################################################################Figure out, whether it is a shell script and if yes execute it accordingly!#######################################################
+				if [[ "$EXEC" =~ ".sh" ]]; then 
+					SLASHNUMBER=$(echo $EXEC  | grep -ho '/' | wc -w)
+					((SLASHNUMBER++))
+					PFADSLASH=""   #variablen initialisierung
+    					PROGRAM="" && echo "found exec sh"
+					APPNAME=./$(echo $EXEC | cut -d/ -f$(($SLASHNUMBER)))
+					echo $APPNAME
+					i=0
+					((SLASHNUMBER--))
+					while [ $i -lt $SLASHNUMBER ]
+						 do   
+							((i++))
+							PFADSLASH=$PFADSLASH$i,
+							echo $PFADSLASH
+						 done
+					PFADSLASH=$(echo $PFADSLASH | sed 's/,$//')
+					echo $PFADSLASH
+					PFAD=$(echo $EXEC | cut -d/ -f$PFADSLASH);
+					echo $PFAD
+					EXEC=$PFAD/$APPNAME			
+					TERMINAL=true
+					ICON=/usr/share/icons/Adwaita/scalable/mimetypes/application-x-executable-symbolic.svg
+				fi
+###############################################################End of the shellscript section, why didn't I use xterm instead of all this, would ve been easier...#######################################				
 		fi
-
 fi
-
-
 #create desktop file and fill it up, open the software with MIME-TYPE
 echo "creating .desktop file"
 touch ~/$NAME.desktop 
 echo "file created"
 echo "[Desktop Entry]"                  >> ~/"$NAME".desktop
 echo "Name=$NAME"                       >> ~/"$NAME".desktop
-echo "Exec=xdg-open ${EXEC// /\\ }"     >> ~/"$NAME".desktop
+echo "Exec=$PROGRAM${EXEC// /\\ }"      >> ~/"$NAME".desktop
 echo "Icon=${ICON// /\\ }"              >> ~/"$NAME".desktop
-echo "Terminal=false"                   >> ~/"$NAME".desktop
+echo "Terminal=$TERMINAL"               >> ~/"$NAME".desktop
 echo "Type=Application"                 >> ~/"$NAME".desktop
 echo "file creation done"
 
